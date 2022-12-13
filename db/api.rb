492fa153aff435ef34440ddf031cd9cf768b43d8
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-12-12 23:39:49 -0500

    added gem rest-client

--- db/api.rb
+++ db/api.rb
@@ -1,10 +1,12 @@
 require "json"
 require "open-uri"
 require "nokogiri"
+require 'rest-client'
 
 def api_call
   Dog.destroy_all
   url = "https://api.petfinder.com/v2/animals?type=dog&age=senior"
+  RestClient.get(url, { "Authorization: Bearer #{ENV['PETFINDER_BEARER']}" })
   user_serialized = URI.open(url).read
   dogs_json = JSON.parse(user_serialized)
   pp dogs = dogs_json['animals']
