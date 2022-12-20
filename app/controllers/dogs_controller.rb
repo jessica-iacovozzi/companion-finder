a9dbdbed2452ece7cf489f9c7eb6fb767a4a50f6
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-12-19 20:35:45 -0500

    moved api call from seeds to controller and placed the call in a service object

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,5 @@
 class DogsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
+  # skip_before_action :authenticate_user!, only: [:show]
 
   def show
     @dog = Dog.find(params[:id])
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  # skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    DogCreator.call
     @dogs = Dog.all
   end
 end
